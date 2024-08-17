8430e967b84f801bb44983b4272322f78e8dbfda
Daniel Franco <danielfranco2011@gmail.com>
2024-08-17 04:41:37 -0700

    Create Pinging ball

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <chrono>
+
 #include <SFML/Graphics.hpp>
 #include "sfml_testing.cpp"
 
@@ -5,8 +7,17 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(1800, 600), "Keyboard Warrior");
 
+    const auto timeToEndScreen{std::chrono::seconds(2)}; // Time it should take for something to go from left-side to right-side
+
+    const auto startTime{std::chrono::steady_clock::now()};
+
+    float inc = 0.f;
+    float x = 0.f;
     while (window.isOpen())
     {
+        if(x >= 1800.f){inc = -5.f;}
+        else if(x<=0){inc = 5.f;}
+        x += inc;
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -15,7 +26,9 @@ int main()
         }
 
         window.clear();
-        Test::drawCircle(100.f, 1800.f / 2, 600.f / 2, window);
+        const auto currentTime{std::chrono::steady_clock::now()};
+        const auto percentage = (currentTime - startTime) / timeToEndScreen; // Perentage of screen traveled
+        Test::drawCircle(100.f, x, 600/2, window);
         Test::drawText("./coolvetica rg.otf", window);
         window.display();
     }
